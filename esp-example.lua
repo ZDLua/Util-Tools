784106c21607fb4755d4356bd230640b8bccbd1f
Zenith <f2v74yt@gmail.com>
2022-07-19 15:48:03 -0500

    Update esp-example.lua

--- esp-example.lua
+++ esp-example.lua
@@ -1,8 +1,7 @@
 local util = loadstring(game:HttpGet("https://raw.githubusercontent.com/AryxVoid/Util-Tools/main/xyz.lua"))()
 
-
 for i,v in pairs(game.Players:GetChildren()) do
-    if v.Name ~= game.Players.LocalPlayer.Name then
-        util.makeEsp(v.Character.Head, Color3.new(1, 0, 0), v.Name, "Arcade")
+        if v.Name ~= game.Players.LocalPlayer.Name then
+            util.makeEsp(v.Character.Head, Color3.new(34, 122, 0), UDim2.new(0, 200, 0, 50), v.Name, "Arial")
+        end
     end
-end

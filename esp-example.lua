0855b1b8e83ee23e1b8cf2b7cc8fec3b9fabdc47
Zenith <f2v74yt@gmail.com>
2022-07-20 08:14:46 -0500

    Update esp-example.lua

--- esp-example.lua
+++ esp-example.lua
@@ -2,6 +2,8 @@ local util = loadstring(game:HttpGet("https://raw.githubusercontent.com/AryxVoid
 
 for i,v in pairs(game.Players:GetChildren()) do
         if v.Name ~= game.Players.LocalPlayer.Name then
-            util.makeEsp(v.Character.Head, Color3.new(34, 122, 0), UDim2.new(0, 200, 0, 50), v.Name, "Arial")
+           util.makeEsp(v.Character.Head, Color3.new(34, 122, 0), UDim2.new(0, 200, 0, 50), v.Name, "Arial", {
+                shake = false -- only enable this if you want the text to have a shaking effect
+            })
         end
     end

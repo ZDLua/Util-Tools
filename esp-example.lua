25815ea96e502b4c1ec421f0b77e74dc65878580
Zenith <f2v74yt@gmail.com>
2022-07-20 14:13:41 -0500

    Update esp-example.lua

--- esp-example.lua
+++ esp-example.lua
@@ -3,7 +3,8 @@ local util = loadstring(game:HttpGet("https://raw.githubusercontent.com/JZRSynta
 for i,v in pairs(game.Players:GetChildren()) do
         if v.Name ~= game.Players.LocalPlayer.Name then
            util.makeEsp(v.Character.Head, Color3.new(34, 122, 0), UDim2.new(0, 200, 0, 50), v.Name, "Arial", {
-                shake = false -- only enable this if you want the text to have a shaking effect
+                 ['shake'] = false -- only enable this if you want the text to have a shaking effect
+                 ['distance'] = false -- only enable this if you want the text to have a studs away counter
             })
         end
     end

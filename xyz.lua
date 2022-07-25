5acd364f70396f208b5683f9ee8417dee4f1518e
Zenith <f2v74yt@gmail.com>
2022-07-25 15:28:42 -0500

    Update xyz.lua

--- xyz.lua
+++ xyz.lua
@@ -174,5 +174,10 @@ util.rejoinGame = function()
     teleport:Teleport(game.PlaceId, player)
 end
 
+util.serverhop = function()
+	local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+
+	module:Teleport(game.PlaceId)		
+end
 
 return util
